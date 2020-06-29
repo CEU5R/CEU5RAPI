5f5b9d56cc6ade1eb2bffa1fce73510c7d3a54d7
jbgaela <jbgaela@gmail.com>
2020-06-29 21:49:20 +0800

    Added unique value to department and buildings

--- models/Building.js
+++ models/Building.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const BuildingSchema = new mongoose.Schema({
-    building: {
-        type: String,
-        required: [true, 'Please add a building'],
-        trim: true,
-        maxlength: [50, 'Building name cannot be more than 50 characters'],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    status: {
-        type: Number,
-        enum: [1, 0],
-        default: 1,
-    },
+  building: {
+    type: String,
+    required: [true, "Please add a building"],
+    trim: true,
+    maxlength: [50, "Building name cannot be more than 50 characters"],
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: Number,
+    enum: [1, 0],
+    default: 1,
+  },
 });
-module.exports = mongoose.model('Building', BuildingSchema);
+module.exports = mongoose.model("Building", BuildingSchema);
--- models/Department.js
+++ models/Department.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const DepartmentSchema = new mongoose.Schema({
-    department: {
-        type: String,
-        required: [true, 'Please add a Department'],
-        trim: true,
-        maxlength: [50, 'Department name cannot be more than 50 characters'],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    status: {
-        type: Number,
-        enum: [1, 0],
-        default: 1,
-    },
+  department: {
+    type: String,
+    required: [true, "Please add a Department"],
+    trim: true,
+    maxlength: [50, "Department name cannot be more than 50 characters"],
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: Number,
+    enum: [1, 0],
+    default: 1,
+  },
 });
-module.exports = mongoose.model('Department', DepartmentSchema);
+module.exports = mongoose.model("Department", DepartmentSchema);
